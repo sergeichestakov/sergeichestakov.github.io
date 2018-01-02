bc69c37af1c94da012d78d12113c2a77b2cda09c
Sergei Chestakov <sergei332@gmail.com>
2018-01-01 23:46:48 -0800

    Various content changes and positioning fixes

--- js/agency.js
+++ js/agency.js
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 200
+    offset: 100
   });
 
   // Collapse Navbar
