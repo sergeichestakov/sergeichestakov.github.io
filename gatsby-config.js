a6ad972ad0afcdc76c68a455584c27d31bc70135
Sergei Chestakov <schestakov@ucdavis.edu>
2019-01-01 18:28:34 -0800

    Update gatsby config and package.json with new metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+    title: "Sergei Chestakov - Portfolio",
+    author: "Sergei Chestakov",
+    description: "Personal Portfolio for Sergei Chestakov"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
