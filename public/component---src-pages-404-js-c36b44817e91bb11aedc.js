f641c639aaafc480041e978d96146e0ac9b782ce
Sergei Chestakov <schestakov@ucdavis.edu>
2019-01-02 20:39:36 -0800

    Remove public folder from index

--- public/component---src-pages-404-js-c36b44817e91bb11aedc.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{135:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),s=n(157);t.default=function(){return u.a.createElement(s.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-c36b44817e91bb11aedc.js.map
