b722b8616a792fbbf1fbb768bd27a08fe8f4a8c1
Sergei Chestakov <schestakov@ucdavis.edu>
2019-01-02 19:29:17 -0800

    Update bg and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#212121',
+        theme_color: '#212121',
         display: 'minimal-ui',
         icon: 'public/icons/favicon.jpg', // This path is relative to the root of the site.
       },
