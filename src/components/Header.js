189451d3b1d67a9c9454870337e326cf2111bf05
Sergei Chestakov <schestakov@ucdavis.edu>
2019-04-19 23:11:40 -0700

    Change profile picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import profile from '../assets/images/sergeichestakov.jpg'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="Profile photo" /></a>
+                    <a href="#" className="image avatar"><img src={profile} alt="Profile photo" /></a>
                     <h1><strong>Hi I'm Sergei </strong><br />
                     <span id="description">CS Student & </span><br />
                     Software Engineer</h1>
