f31cca07617aacc9211ed4baec1c6d195063dac0
Sergei Chestakov <schestakov@ucdavis.edu>
2019-05-04 15:41:18 -0700

    Add Twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,7 @@ class Footer extends React.Component {
                         <li><a href="mailto:schestakov@ucsb.edu" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                         <li><a href="https://github.com/sergeichestakov" className="icon fa-github" target="_blank" rel="noopener noreferrer"><span className="label">Github</span></a></li>
                         <li><a href="https://www.linkedin.com/in/sergeichestakov/" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer"><span className="label">LinkedIn</span></a></li>
+                        <li><a href="https:twitter.com/sergeichestakov" className="icon fa-twitter" target="_blank" rel="noopener noreferrer"><span className="label">Twitter</span></a></li>
                         <li><a href="https://open.spotify.com/user/sergei332" className="icon fa-spotify" target="_blank" rel="noopener noreferrer"><span className="label">Spotify</span></a></li>
                     </ul>
                 </div>
