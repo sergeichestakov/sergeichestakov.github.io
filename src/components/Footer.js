e63ed5dae54e10052246168057c4ac3e11f47422
Sergei Chestakov <schestakov@ucdavis.edu>
2019-05-04 15:48:08 -0700

    Fix Twitter link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="mailto:sergei332@gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                         <li><a href="https://github.com/sergeichestakov" className="icon fa-github" target="_blank" rel="noopener noreferrer"><span className="label">Github</span></a></li>
-                        <li><a href="https://www.linkedin.com/in/sergeichestakov/" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer"><span className="label">LinkedIn</span></a></li>
-                        <li><a href="https:twitter.com/sergeichestakov" className="icon fa-twitter" target="_blank" rel="noopener noreferrer"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://linkedin.com/in/sergeichestakov/" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer"><span className="label">LinkedIn</span></a></li>
+                        <li><a href="https://twitter.com/sergeichestakov" className="icon fa-twitter" target="_blank" rel="noopener noreferrer"><span className="label">Twitter</span></a></li>
                         <li><a href="https://open.spotify.com/user/sergei332" className="icon fa-spotify" target="_blank" rel="noopener noreferrer"><span className="label">Spotify</span></a></li>
                     </ul>
                 </div>
