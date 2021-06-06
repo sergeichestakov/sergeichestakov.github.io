d765547de607092fac601ee44bc609b86e5074ba
sergeichestakov <me@sergei.com>
2021-06-06 14:44:17 -0500

    Use stripe S ;)

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Helmet from "react-helmet";
 import Layout from "../components/layout";
 import Gallery from "../components/Gallery";
 
-import favicon from "../assets/images/favicon.jpg";
+import favicon from "../assets/images/favicon.png";
 
 import expo from "../assets/images/expo.jpg";
 import schedulehelper from "../assets/images/schedule-helper.png";
